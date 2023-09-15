358a7b4919987a56f6ef7facd93bba8d07feeafa
hjuuK <khj020508@naver.com>
2023-09-15 19:16:13 +0900

    0915

--- src/main/java/org/teamproject/configs/MvcConfig.java
+++ src/main/java/org/teamproject/configs/MvcConfig.java
@@ -1,12 +1,18 @@
 package org.teamproject.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.teamproject.configs.interceptors.CommonInterceptor;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private CommonInterceptor commonInterceptor;
+
     @Value("${file.upload.path}")
     private String fileUploadPath;
 
@@ -18,5 +24,11 @@ public class MvcConfig implements WebMvcConfigurer {
         /* 파일 업로드 경로 설정 */
         registry.addResourceHandler(fileUploadUrl + "**") // 하위 경로 전체 해당
                 .addResourceLocations("file:///" + fileUploadPath);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(commonInterceptor)
+                .addPathPatterns("/**");
     }
 }
