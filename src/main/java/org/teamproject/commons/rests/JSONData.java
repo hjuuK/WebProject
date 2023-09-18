975d696d50cf056283b36e3614210f9cf54a31c3
hjuuK <khj020508@naver.com>
2023-09-18 16:29:46 +0900

    초기 사이트 구성

--- src/main/java/org/teamproject/commons/rests/JSONData.java
+++ src/main/java/org/teamproject/commons/rests/JSONData.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @Data @Builder
 @NoArgsConstructor // Lombok 애너테이션: 파라미터 없는 생성자 자동 생성
 @AllArgsConstructor // Lombok 애너테이션: 모든 필드를 파라미터로 받는 생성자 자동 생성
-public class JSONData {
+public class JSONData<T> {
     private boolean success;
     private HttpStatus status = HttpStatus.OK; // HTTP 응답 상태 코드, 기본값은 200 OK
     private String message; // 응답 메시지를 담는 문자열
