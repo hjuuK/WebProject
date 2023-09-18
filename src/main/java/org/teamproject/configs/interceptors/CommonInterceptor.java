afabd37ddf1ca6586ed9999072e695580f6f2571
hjuuK <khj020508@naver.com>
2023-09-18 16:14:04 +0900

    초기 사이트 구성

--- src/main/java/org/teamproject/configs/interceptors/CommonInterceptor.java
+++ src/main/java/org/teamproject/configs/interceptors/CommonInterceptor.java
@@ -1,10 +1,15 @@
 package org.teamproject.configs.interceptors;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.teamproject.commons.configs.ConfigInfoService;
+
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -20,13 +25,30 @@ public class CommonInterceptor implements HandlerInterceptor {
     }
 
     private void init() {
+        HttpSession session = request.getSession();
+        String URI = request.getRequestURI();
+        // 1. 로그인 실패시 세션에 남아 있는 데이터 삭제
+        if (URI.indexOf("/member/login") == -1) {
+            // 로그인이 아닌 경로로 접속한 경우 불필요한 세션 데이터 삭제
+            session.removeAttribute("email");
+            session.removeAttribute("requiredEmail");
+            session.removeAttribute("requiredUserPw");
+            session.removeAttribute("globalError");
+        }
+
+        // 2. 현재 접속한 장비 체크하여 템플릿 분리 설정
         /**
          * PC, MOBILE 구분 -> ?device=
          */
         String device = request.getParameter("device");
         if (device != null) {
+
             device = device.equals("mobile") ? "mobile" : "pc";
+            session.setAttribute("device", device);
         }
 
+        // 3. 사이트 설정 조회
+        Map<String, String> siteConfigs = infoService.get("siteConfig", new TypeReference<Map<String, String>>() {});
+        request.setAttribute("siteConfig", siteConfigs);
     }
 }
