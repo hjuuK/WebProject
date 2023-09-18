c4af2e530b0163c7bb90769072bb3fddbab1b267
hjuuK <khj020508@naver.com>
2023-09-18 16:25:48 +0900

    초기 사이트 구성

--- src/main/java/org/teamproject/configs/SecurityConfig.java
+++ src/main/java/org/teamproject/configs/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.teamproject.models.member.LoginFailureHandler;
+import org.teamproject.models.member.LoginSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
